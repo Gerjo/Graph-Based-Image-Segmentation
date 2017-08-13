159b17bfe45f38476de1a6d424763f03c3ef25a7
Gerard <gerjoo@gmail.com>
2017-08-13 14:33:53 +0100

    newlines and writing to stdout.

--- segment.cpp
+++ segment.cpp
@@ -34,9 +34,9 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 int main(int argc, char **argv) {
 	
   if (argc != 6) {
-    fprintf(stderr, "usage: %s sigma k min input(jpg) output(png)\n", argv[0]);
-    fprintf(stderr, "  Typical parameters are sigma = 0.5, k = 500, min = 20.");
-    fprintf(stderr, "  Larger values for k result in larger components in the result.");
+    fprintf(stdout, "usage: %s sigma k min input(jpg) output(png)\n", argv[0]);
+    fprintf(stdout, "  Typical parameters are sigma = 0.5, k = 500, min = 20.\n");
+    fprintf(stdout, "  Larger values for k result in larger components in the result.\n");
 
     return 1;
   }
